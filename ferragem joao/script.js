b8bc042b0ed45ed3bfbb6d6414c75d4238297614
unknown <vitoria.a.lima102aluno.senai.br!>
2025-04-15 10:51:12 -0500

    alteração da atividade

--- ferragem joao/script.js
+++ ferragem joao/script.js
@@ -1,19 +1,41 @@
 //MARTELO,PREGO,ALICATE,PARAFUSO
 // 19,90  10,00  23,00 
 
-const precos={
+const precos={ //DICIONARIO
     "parafuso": 10.90,
     "martelo":  23.70,
     "chave de fenda": 10.99,
     "serrote": 30.00
 };
-//atualizar campo de preço automaticamente ao escolher produto 
-document.getElementById("produto").addEventListener("change,fucction()"){
-    const produtoSelecionado = this.ariavaluemax;
-    const precos =
-    precos 
-    [produtosselecionados] || "" ;
-    document.getElementById("preco").value = precos ? precos.tofixed(2):"";     
+
+const estoque={ 
+    "parafuso": 100,
+    "martelo":  4,
+    "chave de fenda": 6,
+    "serrote": 1000,
+
+};
+//ACESSAR GUARDAR A QUANTIDADE 
+//CALCULAR O PREÇO TOTAL 
+function calcularpreco(){
+    
+    let qtd = document.getElementById("quantidade").value; //pega a quantidade atual digitada
+    let escolha = document.getElementById("produto").value; //pega o nome do produto selecionado 
+    let valortxoal = qtd * precos[escolha] //calcular o valor total multiplicando o preço do produto pela quantidade 
+    document.getElementById("resultado").innerHTML = valortxoal // 
 }
-     
+
+let qtd = document.getElementById("quantidade").Value;
+
+comsole.log(qtd);
+
+
+//CALCULAR O PREÇO TOTAL 
+function calcularpreco(){
+    let qtd = document.getElementById("quantidade").value; //pega a quantidade atual digitada
+    let escolha = document.getElementById("produto").value; //pega o nome do produto selecionado 
+    let valortxoal = qtd * precos[escolha] //calcular o valor total multiplicando o preço do produto pela quantidade 
+    document.getElementById("resultado").innerHTML = valortxoal // 
+}
+
 
